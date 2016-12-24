6698b54f62b9928b7a9ac34132c2f79ca03cba7e
Chuehdx <dxcrabby@gmail.com>
2016-12-25 02:58:48 +0800

    better module

--- myAES.h
+++ myAES.h
@@ -6,14 +6,14 @@
 
 struct AESCrypt{
 	char filename[20],encryptedfilename[30],decryptedfilename[30];
-	EVP_CIPHER_CTX *de;
+	EVP_CIPHER_CTX *en,*de;
 };
 
 int aes_init(unsigned char* password, unsigned int password_len, unsigned char * salt, EVP_CIPHER_CTX *e_ctx, EVP_CIPHER_CTX *d_ctx);
 
-int aes_encrypt(EVP_CIPHER_CTX *e,int inputfile,int encryptedfile);
+int aes_encrypt(char* filename);
 
-int aes_decrypt(EVP_CIPHER_CTX *d, int encryptedfile,int outputfile);
+int aes_decrypt(char* filename);
 
 void generate_new_password(unsigned char* password);
 
